c350e9642d749290dccf2754fce96a290fa457b3
gorlova-natalya <natashka26102013@gmail.com>
2022-10-23 19:06:11 +0300

    lesson 27 Commit

--- lesson27/src/main/java/com/teachmeskills/model/User.java
+++ lesson27/src/main/java/com/teachmeskills/model/User.java
@@ -1,27 +1,10 @@
 package com.teachmeskills.model;
 
-import lombok.AllArgsConstructor;
+import lombok.Value;
 
-@AllArgsConstructor
+@Value
 public class User {
 
-    private String login;
-    private String password;
-
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+     String login;
+     String password;
 }
